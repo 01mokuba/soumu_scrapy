9570061956db0026661b9b0adc4df6c8a611836a
vagrant <vagrant@localhost.localdomain>
2018-09-09 07:37:53 +0000

    enable archive scraping

--- soumu_scrapy/spiders/archive.py
+++ soumu_scrapy/spiders/archive.py
@@ -5,26 +5,26 @@ from soumu_scrapy.items import ArchiveItem, ClipItem
 
 class ArchiveSpider(CrawlSpider):
     name = 'archive'
-    allowed_domains = ['http://www.soumu.go.jp/']
+    allowed_domains = ['www.soumu.go.jp']
     start_urls = ['http://www.soumu.go.jp/menu_news/s-news/index.html']
     custom_settings = {
         'DOWNLOAD_DELAY' : 1,
     }
     rules = (
         Rule(LinkExtractor(
-                allow=['http://www.soumu.go.jp/menu_news/s-news/[\d]+m.html'],
-                restrict_xpaths=['//table[@class=\"tableList\"]']
-            ),callback='parse_archive_list', follow=True
-        )
+                allow=['http://www.soumu.go.jp/menu_news/s-news/[\d]+m\.html'],
+                restrict_xpaths=['//div[@class=\'contentsBody\']']
+            ), callback='parse_archive_list', follow=True),
     )
 
     def parse_archive_list(self, response):
         item = ArchiveItem()
         item['links'] = []
         item['month'] = response.url.split('/')[-1].replace('m.html','')
-        for linkitem in response.xpath('//table[@class=\"tableList\"]//a'):
+        for linkitem in response.xpath('//div[@class=\"contentsBody\"]//a'):
             item['links'].append({
                 'href' : linkitem.xpath('@href').extract_first(),
                 'text' : linkitem.xpath('text()').extract_first()
             })
+
         return item
