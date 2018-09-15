baf0cac1f6c4f795e854c66f5b70e5ae77b206d3
yuonoda <yu.ond.for.pc3934@gmail.com>
2018-09-15 07:01:18 +0000

    created detail method

--- soumu_scrapy/spiders/archive.py
+++ soumu_scrapy/spiders/archive.py
@@ -11,13 +11,22 @@ class ArchiveSpider(CrawlSpider):
         'DOWNLOAD_DELAY' : 1,
     }
     rules = (
-        Rule(LinkExtractor(
-            allow=['http://www.soumu.go.jp/menu_news/s-news/[\d]+m\.html'], #リンク抽出をするURL - 月ごとの報道資料一覧
-                restrict_xpaths=['//div[@class=\'contentsBody\']'] #リンク抽出をするエリア
-            ), 
-        	callback='parse_archive_list', #リンク抽出後に実行されるコールバック
-            follow=True
+        Rule(
+                LinkExtractor(
+                    allow=['http://www.soumu.go.jp/menu_news/s-news/[\d]+m\.html'], #リンク抽出をするURL - 月ごとの報道資料一覧
+                    restrict_xpaths=['//div[@class=\'contentsBody\']'] #リンク抽出をするエリア
+                ), 
+                callback='parse_archive_list', #リンク抽出後に実行されるコールバック
+                follow=True
             ),
+        Rule(
+                LinkExtractor(
+                    allow=['http://www.soumu.go.jp/menu_news/s-news/[\d\w]+\.html'], #リンク抽出をするURL - 報道資料詳細
+                    restrict_xpaths=['//div[@class=\'contentsBody\']'] #リンク抽出をするエリア
+                ), 
+                callback='parse_archive_detail', #リンク抽出後に実行されるコールバック
+                follow=True
+            )
     )
 
     def parse_archive_list(self, response):
@@ -29,5 +38,7 @@ class ArchiveSpider(CrawlSpider):
                 'href' : linkitem.xpath('@href').extract_first(), #URLを抽出
                 'text' : linkitem.xpath('text()').extract_first() #アンカーテキストを抽出
             })
-
         return item
+
+    def parse_archive_detail():
+        pass
